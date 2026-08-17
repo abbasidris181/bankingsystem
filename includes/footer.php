53766e82827dc121a610cea018d132213573bd40
abbasidris181 <abbasidris2019@gmail.com>
2026-08-16 19:03:36 -0700

    Implement customer creation and database persistence

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,32 @@
     All Rights Reserved.
 
 </footer>
+        <?php if (isset($_SESSION["success"])): ?>
 
+        <script>
+
+            iziToast.success({
+
+                title: "Success",
+
+                message:
+                    <?= json_encode($_SESSION["success"]) ?>,
+
+                position: "topRight",
+
+                timeout: 4000
+
+            });
+
+        </script>
+
+        <?php
+
+        unset($_SESSION["success"]);
+
+        endif;
+
+        ?>
 </body>
 
 </html>
